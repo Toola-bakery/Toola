33224633c040f35a95f7e84a650c369b88642823
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-07-10 12:41:05 +0300

    init with tabler

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Button } from "tabler-react";
+import "tabler-react/dist/Tabler.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
+      <Card.Header>
+        <Card.Title>Card Title</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Button color="primary">A Button</Button>
+      </Card.Body>
+    </Card>
   );
 }
 
