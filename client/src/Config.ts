d29b1748e8e661125bc408b2dfddfacd5c1a078f
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-10-26 23:20:16 +0300

    TOOLA.SO

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -1,8 +1,4 @@
 export const Config = {
-	domain: window.location.host.includes('localhost')
-		? 'http://localhost:8080'
-		: 'https://workspace-5wcfu.ondigitalocean.app',
-	websocket: window.location.host.includes('localhost')
-		? 'ws://localhost:8080'
-		: 'wss://workspace-5wcfu.ondigitalocean.app',
+	domain: window.location.host.includes('localhost') ? 'http://localhost:8080' : 'https://api.toola.so',
+	websocket: window.location.host.includes('localhost') ? 'ws://localhost:8080' : 'wss://api.toola.so',
 };
