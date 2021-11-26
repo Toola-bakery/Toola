655a5b2e7815ef550935da4cd3ed8a12ff4ba5c5
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-11-26 19:08:18 +0300

    Card Background

--- client/src/features/editor/components/Blocks/CardBlock.tsx
+++ client/src/features/editor/components/Blocks/CardBlock.tsx
@@ -1,5 +1,7 @@
 import { Button, Card } from '@blueprintjs/core';
 import React, { useMemo } from 'react';
+import { MenuItemProps } from '../../../inspector/components/InspectorItem';
+import { useAppendBlockMenu } from '../../hooks/blockInspector/useAppendBlockMenu';
 import { useBlock } from '../../hooks/useBlock';
 import { useBlockContext } from '../../hooks/useBlockContext';
 import { useBlockProperty } from '../../hooks/useBlockProperty';
@@ -12,14 +14,34 @@ export function CardBlock({ hide }: { hide: boolean }) {
 	const block = useBlock();
 	const { showInspector } = useBlockContext();
 	const [blocks] = useBlockProperty<string[]>('blocks', []);
-
+	const [backgroundColor, setBackgroundColor] = useBlockProperty<string>('backgroundColor');
+	const menu = useMemo<MenuItemProps[]>(
+		() => [
+			{
+				type: 'input',
+				value: backgroundColor || '',
+				onChange: setBackgroundColor,
+				label: 'Background color',
+			},
+		],
+		[backgroundColor, setBackgroundColor],
+	);
+	useAppendBlockMenu(menu, 0);
 	if (hide || !block.show) return null;
 
 	return (
 		<div onContextMenu={showInspector}>
-			<Card style={{ paddingTop: 25, paddingRight: 25, paddingBottom: 25, paddingLeft: 0 }}>
+			<Card
+				style={{
+					paddingTop: 20,
+					paddingRight: 20,
+					paddingBottom: 20,
+					paddingLeft: 0,
+					backgroundColor: backgroundColor || 'white',
+				}}
+			>
 				<div style={{ display: 'flex', alignItems: 'center', paddingLeft: 25 }}>
-					<EmojiPicker useDefaultDocument={false} />
+					<EmojiPicker hideIfEmpty useDefaultDocument={false} />
 					<EditableText placeholder="Untitled" tagName="h4" className="bp4-heading" />
 				</div>
 				<ColumnBlock fake block={block as BasicBlock & ColumnBlockType} />
