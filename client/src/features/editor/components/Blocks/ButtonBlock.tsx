8b0d08e2b80de55a560d8c52e186a1da8cd0b4d5
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-11-26 19:07:59 +0300

    Rounded & minimal Button

--- client/src/features/editor/components/Blocks/ButtonBlock.tsx
+++ client/src/features/editor/components/Blocks/ButtonBlock.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@blueprintjs/core';
 import React, { useMemo } from 'react';
+import styled from 'styled-components';
 import { usePageNavigator } from '../../../../hooks/usePageNavigator';
 import { MenuItemProps } from '../../../inspector/components/InspectorItem';
 import { usePageModal } from '../../../pageModal/hooks/usePageModal';
@@ -19,6 +20,12 @@ export type ButtonBlockProps = {
 	name: string;
 };
 
+const RoundedButton = styled(Button)<{ rounded?: boolean }>`
+	&.rounded {
+	}
+	border-radius: ${(props) => (props.rounded ? '30px' : '3px')};
+`;
+
 export function ButtonBlock({ hide }: { hide: boolean }) {
 	const { show } = useBlock();
 	const { showInspector } = useBlockContext();
@@ -30,6 +37,14 @@ export function ButtonBlock({ hide }: { hide: boolean }) {
 	const [customCode, setCustomCode] = useBlockProperty('customCode', '');
 	const [selectedPage, setSelectedPage] = useBlockProperty('selectedPage', '');
 	const [pageParams, setPageParams] = useBlockProperty('pageParams', '');
+	// eslint-disable-next-line no-template-curly-in-string
+	const [rounded, setRounded] = useBlockProperty<string>('rounded', '${false}');
+	const roundedCalculated = useReferences(rounded);
+
+	// eslint-disable-next-line no-template-curly-in-string
+	const [minimal, setMinimal] = useBlockProperty<string>('minimal', '${false}');
+	const minimalCalculated = useReferences(minimal);
+
 	const { evaluate } = useReferenceEvaluator();
 	const valueCalculated = useReferences(buttonName);
 	const { navigate } = usePageNavigator();
@@ -44,53 +59,73 @@ export function ButtonBlock({ hide }: { hide: boolean }) {
 				value: buttonName,
 			},
 			{
-				label: 'Action type',
-				type: 'select',
-				options: ['Trigger query', 'Navigate page', 'Open modal', 'Custom code'],
-				onChange: (v) => setActionType(v),
-				value: actionType,
+				type: 'nested',
+				label: 'Event Handlers',
+				icon: 'select',
+				next: [
+					{
+						label: 'Action type',
+						type: 'select',
+						options: ['Trigger query', 'Navigate page', 'Open modal', 'Custom code'],
+						onChange: (v) => setActionType(v),
+						value: actionType,
+					},
+					{
+						label: 'Trigger',
+						type: 'input',
+						hide: actionType !== 'Custom code',
+						onChange: (v: string) => setCustomCode(v),
+						value: customCode,
+					},
+					{
+						label: 'Select query',
+						type: 'querySelector',
+						hide: actionType !== 'Trigger query',
+						onChange: (v: string) => setQuery(v),
+						value: query,
+					},
+					{
+						label: 'Select page',
+						type: 'pages',
+						hide: !['Navigate page', 'Open modal'].includes(actionType),
+						onChange: (v: string) => setSelectedPage(v),
+						value: selectedPage,
+					},
+					{
+						label: 'Page params',
+						type: 'input',
+						hide: !['Navigate page', 'Open modal'].includes(actionType),
+						onChange: (v: string) => setPageParams(v),
+						value: pageParams,
+					},
+				],
 			},
 			{
-				label: 'Trigger',
-				type: 'input',
-				hide: actionType !== 'Custom code',
-				onChange: (v: string) => setCustomCode(v),
-				value: customCode,
-			},
-			{
-				label: 'Select query',
-				type: 'querySelector',
-				hide: actionType !== 'Trigger query',
-				onChange: (v: string) => setQuery(v),
-				value: query,
-			},
-			{
-				label: 'Select page',
-				type: 'pages',
-				hide: !['Navigate page', 'Open modal'].includes(actionType),
-				onChange: (v: string) => setSelectedPage(v),
-				value: selectedPage,
-			},
-			{
-				label: 'Page params',
-				type: 'input',
-				hide: !['Navigate page', 'Open modal'].includes(actionType),
-				onChange: (v: string) => setPageParams(v),
-				value: pageParams,
+				type: 'nested',
+				label: 'Appearance',
+				icon: 'style',
+				next: [
+					{ type: 'input', value: minimal, onChange: setMinimal, label: 'Minimal' },
+					{ type: 'input', value: rounded, onChange: setRounded, label: 'Rounded' },
+				],
 			},
 		],
 		[
 			actionType,
 			buttonName,
 			customCode,
+			minimal,
 			pageParams,
 			query,
+			rounded,
 			selectedPage,
 			setActionType,
 			setButtonName,
 			setCustomCode,
+			setMinimal,
 			setPageParams,
 			setQuery,
+			setRounded,
 			setSelectedPage,
 		],
 	);
@@ -102,8 +137,10 @@ export function ButtonBlock({ hide }: { hide: boolean }) {
 	return (
 		<>
 			<div onContextMenu={showInspector}>
-				<Button
+				<RoundedButton
 					fill
+					rounded={roundedCalculated === true}
+					minimal={minimalCalculated === true}
 					onClick={() => {
 						if (actionType === 'Trigger query') {
 							console.log(query, blocks[query]);
