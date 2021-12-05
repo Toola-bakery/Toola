033609a289fafbc1e55316d65428a6b44d5bb875
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-12-06 01:26:41 +0300

    Fix account creation

--- client/src/features/drawer/hooks/useRelocateToAnyPageByCondition.ts
+++ client/src/features/drawer/hooks/useRelocateToAnyPageByCondition.ts
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
 import { usePageNavigator } from '../../../hooks/usePageNavigator';
+import { usePagesMutations } from '../../editor/components/Page/hooks/usePagesMutations';
 import { useTopLevelPages } from './useTopLevelPages';
 
 export function useRelocateToAnyPageByCondition(needRelocate: boolean) {
 	const { pages } = useTopLevelPages();
 	const { navigate } = usePageNavigator();
+	const { createPage } = usePagesMutations();
 
 	useEffect(() => {
-		if (pages[0] && needRelocate) navigate(pages[0].id);
-	}, [pages, navigate, needRelocate]);
+		if (needRelocate) {
+			if (pages[0]) navigate(pages[0].id);
+			else createPage();
+		}
+	}, [pages, navigate, needRelocate, createPage]);
 }
--- repl/src/utils/serviceAccountKey.ts
+++ repl/src/utils/serviceAccountKey.ts
@@ -2,7 +2,7 @@ export default {
 	type: 'service_account',
 	project_id: 'toolspace-d6f56',
 	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-	private_key: process.env.FIREBASE_PRIVATE_KEY,
+	private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 	client_email: 'firebase-adminsdk-235h4@toolspace-d6f56.iam.gserviceaccount.com',
 	client_id: '106637621867445651359',
 	auth_uri: 'https://accounts.google.com/o/oauth2/auth',
